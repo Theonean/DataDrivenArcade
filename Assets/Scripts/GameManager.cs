d01eefc9582913502bad648b37b375550fb5e721
Robin <robinwelmers@protonmail.com>
2024-03-01 18:52:54 +0000

    Moved Time Left for game to game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,25 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
-public enum GameState {
-    INTRO,
-    PLAYING
-}
-
-public enum ColourState{
-    GREEN = 1,
-    RED = 2,
-    BLUE = 3,
-    YELLOW = 4
+public enum CurrentScene{
+    LOGIN,
+    GAMESELECTION,
+    GAMECLASSIC,
+    GAMEFACTORY
 }
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
+    public CurrentScene gameState;
 
-    public ColourState Player1Colour;
-    public ColourState Player2Colour;
+    public float roundTime = 60f;
+    public TextMeshProUGUI countdownTimer;
+
+    private float timeLeft;
 
     void Awake() {
 
@@ -29,28 +28,18 @@ public class GameManager : MonoBehaviour
         DontDestroyOnLoad(this);
     }
 
-    void Start() {
-        //Set default colours from playerprefs
-        Player1Colour = (ColourState)PlayerPrefs.GetInt("Player1Colour");
-        Player2Colour = (ColourState)PlayerPrefs.GetInt("Player2Colour");
+    private void Start() {
+        timeLeft = roundTime;
     }
 
-    public void SetPlayer1Colour(ColourState colour) {
-        Player1Colour = colour;
-        PlayerPrefs.SetInt("Player1Colour", (int)colour);
-    }
-
-    public void SetPlayer2Colour(ColourState colour) {
-        Player2Colour = colour;
-        PlayerPrefs.SetInt("Player2Colour", (int)colour);
-    }
-
-    public ColourState GetPlayerColour(bool isPlayer1) {
-        if (isPlayer1) {
-            return Player1Colour;
-        }
-        else {
-            return Player2Colour;
+    private void Update() {
+        if (gameState == CurrentScene.GAMECLASSIC || gameState == CurrentScene.GAMEFACTORY) {
+            timeLeft -= Time.deltaTime;
+            countdownTimer.text = timeLeft.ToString("F2");
+            if (timeLeft < 0) {
+                timeLeft = roundTime;
+                //End Game
+            }
         }
     }
 
