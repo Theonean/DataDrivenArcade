82d2ebbf0d1c11f8bf10856682b81e4f7ab2966a
Robin <robinwelmers@protonmail.com>
2024-05-14 18:24:20 +0100

    added script to Welcome Scene to change scenes to main menu on insertcoinpressed

---
+++ Assets/Scripts/UI/GameWelcome.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameWelcome : MonoBehaviour
+{
+    private GameManager gm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gm = GameManager.instance;
+        gm.InsertCoinPressed.AddListener(InsertCoinPressed);
+    }
+
+    private void InsertCoinPressed(bool isArcadeMode)
+    {
+        gm.SwitchScene(CurrentScene.LOGIN);
+    }
+}
